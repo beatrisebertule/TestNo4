42850e0be667e4bedbb1d492a7a94f4860556f5a
rknipse <61231928+rknipse@users.noreply.github.com>
2022-02-01 15:49:15 +0200

    Chemistry is like cooking (just don't lick the spoon)

--- TestNo4/Form1.Designer.cs
+++ TestNo4/Form1.Designer.cs
@@ -31,6 +31,14 @@ namespace TestNo4
         {
             this.buttonbea = new System.Windows.Forms.Button();
             this.labelbea = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.buttonExtra = new System.Windows.Forms.Button();
+            this.buttonExtra2 = new System.Windows.Forms.Button();
+            this.buttonExtra3 = new System.Windows.Forms.Button();
+            this.buttonExtra4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonbea
@@ -52,11 +60,107 @@ namespace TestNo4
             this.labelbea.TabIndex = 1;
             this.labelbea.Text = "nekas";
             // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(170, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Kas tad tas?";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.Color.Blue;
+            this.button3.Location = new System.Drawing.Point(274, 169);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Kas tad tas?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.Color.Lime;
+            this.button4.Location = new System.Drawing.Point(66, 169);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 42);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Kas tad tas?";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // buttonExtra
+            // 
+            this.buttonExtra.Location = new System.Drawing.Point(375, 350);
+            this.buttonExtra.Name = "buttonExtra";
+            this.buttonExtra.Size = new System.Drawing.Size(75, 23);
+            this.buttonExtra.TabIndex = 5;
+            this.buttonExtra.Text = "Extra";
+            this.buttonExtra.UseVisualStyleBackColor = true;
+            this.buttonExtra.Click += new System.EventHandler(this.buttonExtra_Click);
+            // 
+            // buttonExtra2
+            // 
+            this.buttonExtra2.Location = new System.Drawing.Point(375, 12);
+            this.buttonExtra2.Name = "buttonExtra2";
+            this.buttonExtra2.Size = new System.Drawing.Size(75, 23);
+            this.buttonExtra2.TabIndex = 6;
+            this.buttonExtra2.Text = "Extra2";
+            this.buttonExtra2.UseVisualStyleBackColor = true;
+            this.buttonExtra2.Visible = false;
+            this.buttonExtra2.Click += new System.EventHandler(this.buttonExtra2_Click);
+            // 
+            // buttonExtra3
+            // 
+            this.buttonExtra3.Location = new System.Drawing.Point(13, 12);
+            this.buttonExtra3.Name = "buttonExtra3";
+            this.buttonExtra3.Size = new System.Drawing.Size(75, 23);
+            this.buttonExtra3.TabIndex = 7;
+            this.buttonExtra3.Text = "Extra3";
+            this.buttonExtra3.UseVisualStyleBackColor = true;
+            this.buttonExtra3.Visible = false;
+            this.buttonExtra3.Click += new System.EventHandler(this.buttonExtra3_Click);
+            // 
+            // buttonExtra4
+            // 
+            this.buttonExtra4.Location = new System.Drawing.Point(13, 350);
+            this.buttonExtra4.Name = "buttonExtra4";
+            this.buttonExtra4.Size = new System.Drawing.Size(75, 23);
+            this.buttonExtra4.TabIndex = 8;
+            this.buttonExtra4.Text = "Extra4";
+            this.buttonExtra4.UseVisualStyleBackColor = true;
+            this.buttonExtra4.Visible = false;
+            this.buttonExtra4.Click += new System.EventHandler(this.buttonExtra4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.ForeColor = System.Drawing.Color.Purple;
+            this.label1.Location = new System.Drawing.Point(12, 335);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 39);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "VISS!";
+            this.label1.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(453, 376);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonExtra4);
+            this.Controls.Add(this.buttonExtra3);
+            this.Controls.Add(this.buttonExtra2);
+            this.Controls.Add(this.buttonExtra);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.labelbea);
             this.Controls.Add(this.buttonbea);
             this.Name = "Form1";
@@ -70,6 +174,14 @@ namespace TestNo4
 
         private System.Windows.Forms.Button buttonbea;
         private System.Windows.Forms.Label labelbea;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buttonExtra;
+        private System.Windows.Forms.Button buttonExtra2;
+        private System.Windows.Forms.Button buttonExtra3;
+        private System.Windows.Forms.Button buttonExtra4;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- TestNo4/Form1.cs
+++ TestNo4/Form1.cs
@@ -21,6 +21,47 @@ namespace TestNo4
         private void buttonbea_Click(object sender, EventArgs e)
         {
             labelbea.Visible = true;
+            labelbea.ForeColor = System.Drawing.Color.Black;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            labelbea.ForeColor = System.Drawing.Color.Green;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            labelbea.ForeColor = System.Drawing.Color.Blue;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            labelbea.ForeColor = System.Drawing.Color.Red;
+
+        }
+
+        private void buttonExtra_Click(object sender, EventArgs e)
+        {
+            buttonExtra2.Visible = true;
+        }
+
+        private void buttonExtra2_Click(object sender, EventArgs e)
+        {
+            buttonExtra3.Visible = true;
+        }
+
+        private void buttonExtra3_Click(object sender, EventArgs e)
+        {
+            buttonExtra4.Visible = true;
+        }
+
+        private void buttonExtra4_Click(object sender, EventArgs e)
+        {
+            buttonExtra.Visible = false;
+            buttonExtra2.Visible = false;
+            buttonExtra3.Visible = false;
+            buttonExtra4.Visible = false;
+            label1.Visible = true;
         }
     }
 }
